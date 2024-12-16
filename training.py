316ab1742e977db0b7b18f2661c4e96e29896e70
ligaydima <ligaydima@list.ru>
2024-12-17 00:58:14 +0300

    add smooth metrics

--- training.py
+++ training.py
@@ -7,6 +7,7 @@ from vis import visualize_training
 from IPython.display import clear_output
 import wandb
 
+
 def train(model, opt, train_dataloader, loss_fn, n_epochs, sampling_params, checkpoint_dir, eval_every=100,
           save_every=1000):
     loss_history = []
@@ -26,7 +27,7 @@ def train(model, opt, train_dataloader, loss_fn, n_epochs, sampling_params, chec
                 grad_history.append(get_grad_norm(model).detach().cpu())
                 if it % eval_every == 0:
                     model.eval()
-                    
+
                     clear_output(wait=True)
                     visualize_training(model, loss_history, grad_history, log_imgs, sampling_params)
 
@@ -35,6 +36,13 @@ def train(model, opt, train_dataloader, loss_fn, n_epochs, sampling_params, chec
                 pbar.update(1)
                 pbar.set_description('Loss: %.4g' % loss.item())
                 it += 1
-                wandb.log({"loss": loss_history[-1], "grad_norm": grad_history[-1]})
+                smoothened_loss = sum(loss_history[max(0, len(loss_history) - 100):]) / min(100, len(loss_history))
+                smoothened_grad = sum(grad_history[max(0, len(grad_history) - 100):]) / min(100, len(loss_history))
+                wandb.log({
+                    "loss": loss_history[-1],
+                    "grad_norm": grad_history[-1],
+                    "smooth_loss": smoothened_loss,
+                    "smooth_grad": smoothened_grad
+                })
 
     return model, loss_history, grad_history
