473ff98fd36fe586852f465773461a0e6c64cc28
ligaydima <ligaydima@list.ru>
2024-12-17 10:17:06 +0300

    fix checkpoints saving

--- run_train.py
+++ run_train.py
@@ -23,4 +23,4 @@ def run_train(params):
     loss_fn = FMLoss(sampling_params, params["batch_size"], distr='logit', use_OT=params["use_OT"])
 
     model, loss_log, grad_log = train(model, optimizer, dataset.get_dataloader(params["batch_size"]), loss_fn, params["n_epochs"],
-                                      sampling_params, params["checkpoint_dir"])
+                                      sampling_params, params["checkpoint_dir"], eval_every=params["eval_every"], save_every=params["save_every"])
