47094523ca8028c10b8ca2aaddfc54841c96a158
ligaydima <ligaydima@list.ru>
2024-12-18 00:04:54 +0300

    addd mnist

--- dataset.py
+++ dataset.py
@@ -1,9 +1,10 @@
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
-
+import torch
 CIFAR10_ROOT = './data'
 MNIST_ROOT = './data_mnist'
+
 
 def get_dataloader(batch_size):
     import ssl
@@ -16,6 +17,56 @@ def get_dataloader(batch_size):
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     return train_loader
 
+
+class ColoredMNIST(datasets.MNIST):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.hues = 360 * torch.rand(super().__len__())
+
+    def __len__(self):
+        return super().__len__()
+
+    def color_image(self, img, idx):
+        img_min = 0
+        a = (img - img_min) * (self.hues[idx] % 60) / 60
+        img_inc = a
+        img_dec = img - a
+
+        colored_image = torch.zeros((3, img.shape[1], img.shape[2]))
+        H_i = round(self.hues[idx].item() / 60) % 6
+
+        if H_i == 0:
+            colored_image[0] = img
+            colored_image[1] = img_inc
+            colored_image[2] = img_min
+        elif H_i == 1:
+            colored_image[0] = img_dec
+            colored_image[1] = img
+            colored_image[2] = img_min
+        elif H_i == 2:
+            colored_image[0] = img_min
+            colored_image[1] = img
+            colored_image[2] = img_inc
+        elif H_i == 3:
+            colored_image[0] = img_min
+            colored_image[1] = img_dec
+            colored_image[2] = img
+        elif H_i == 4:
+            colored_image[0] = img_inc
+            colored_image[1] = img_min
+            colored_image[2] = img
+        elif H_i == 5:
+            colored_image[0] = img
+            colored_image[1] = img_min
+            colored_image[2] = img_dec
+
+        return colored_image
+
+    def __getitem__(self, idx):
+        img, label = super().__getitem__(idx)
+        return self.color_image(img, idx), label
+
+
 def get_dataloader_mnist(batch_size):
     import ssl
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -23,6 +74,6 @@ def get_dataloader_mnist(batch_size):
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # [-1, 1]
     ])
-    train_dataset = datasets.MNIST(root=MNIST_ROOT, train=True, download=True, transform=transform)
+    train_dataset = ColoredMNIST(root=MNIST_ROOT, train=True, download=True, transform=transform)
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     return train_loader
